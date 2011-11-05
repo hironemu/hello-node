fea799faa47c057d3ba1dcfcff48643cda5831a6
hironemu <hiromi.satake@gmail.com>
2011-11-06 00:46:27 +0900

    add module

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-var http = require('http');
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello node!\n');
-}).listen(8080);
+var express = require('express');
+var app = express.createServer();
+app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.route);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res) {
+    res.render('index', {
+	title: 'Express'
+    });
+});
+app.listen(8080);
