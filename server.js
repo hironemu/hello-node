bcd7cde582204d03d76c42cafebf78baf984feb9
hironemu <hiromi.satake@gmail.com>
2011-11-12 20:57:35 +0900

    change server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express.createServer();
+var io = require('socket.io').listen(app);
+
 app.configure(function() {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -13,6 +15,17 @@ app.configure(function() {
 app.get('/', function(req, res) {
     res.render('index', {
 	title: 'Express'
+    });
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function(data) {
+	console.log(data);
+    });
+    socket.on('send_message', function(data) {
+	console.log('receive message: ' + data.message);
+	socket.broadcast.emit('broadcastMessage', data.message);
     });
 });
 app.listen(8080);
